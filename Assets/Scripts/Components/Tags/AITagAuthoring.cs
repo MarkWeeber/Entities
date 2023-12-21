26379c5553b62acc963c86077142d2feb7adab3f
Mark Weeber <m.webber@inbox.ru>
2023-12-22 00:24:25 +0600

    testing stuff with entities rotation

---
+++ Assets/Scripts/Components/Tags/AITagAuthoring.cs
@@ -0,0 +1,15 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class AITagAuthoring : MonoBehaviour
+{
+
+	class Baker : Baker<AITagAuthoring>
+	{
+		public override void Bake(AITagAuthoring authoring)
+		{
+			Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+			AddComponent(entity, new AITag { });
+		}
+	}
+}
