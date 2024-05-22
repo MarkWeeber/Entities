c6a07c612f1d530833e6d5eaf4a65154eb786c1d
Mark Weeber <m.webber@inbox.ru>
2024-05-22 16:48:30 +0600

    trying to work with IJobChunk

--- Assets/Scripts/Systems/SpawnPerInstruction/SpawnPerInstructionSystem.cs
+++ Assets/Scripts/Systems/SpawnPerInstruction/SpawnPerInstructionSystem.cs
@@ -5,17 +5,20 @@ using Unity.Burst.Intrinsics;
 using Unity.Transforms;
 using Unity.Mathematics;
 using Unity.Physics;
+using UnityEngine;
 
 [BurstCompile]
 [UpdateInGroup(typeof(InitializationSystemGroup))]
 public partial struct SpawnPerInstructionSystem : ISystem
 {
     public BufferTypeHandle<SpawnInstructionBuffer> spawnInstructionsTypeHandle;
+    public EntityTypeHandle entityTypeHandle;
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<SpawnInstructionBuffer>();
         spawnInstructionsTypeHandle = state.GetBufferTypeHandle<SpawnInstructionBuffer>();
+        entityTypeHandle = state.GetEntityTypeHandle();
     }
     [BurstCompile]
     public void OnDestroy(ref SystemState state)
@@ -24,32 +27,53 @@ public partial struct SpawnPerInstructionSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        state.Enabled = false;
         var entities = SystemAPI.QueryBuilder().WithAll<SpawnInstructionBuffer>().Build();
         var ecb = new EntityCommandBuffer(Allocator.TempJob);
         spawnInstructionsTypeHandle.Update(ref state);
+        entityTypeHandle.Update(ref state);
         CollisionWorld collisionWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().CollisionWorld;
+        var chunkBaseEntityIndices = entities.CalculateBaseEntityIndexArray(Allocator.TempJob);
         var jobHandle = new SpawnPerInstructionJob
         {
             SpawnInstructionsTypeHandle = spawnInstructionsTypeHandle,
             ECB = ecb,
-            CollisionWorld = collisionWorld
+            CollisionWorld = collisionWorld,
+            ETH = entityTypeHandle,
+            ChunkBaseEntityIndices = chunkBaseEntityIndices
         }.Schedule(entities, state.Dependency);
         jobHandle.Complete();
         ecb.Playback(state.EntityManager);
         ecb.Dispose();
+        chunkBaseEntityIndices.Dispose();
     }
 
     [BurstCompile]
     private partial struct SpawnPerInstructionJob : IJobChunk
     {
+        public NativeArray<int> ChunkBaseEntityIndices;
         public BufferTypeHandle<SpawnInstructionBuffer> SpawnInstructionsTypeHandle;
         public EntityCommandBuffer ECB;
+        public EntityTypeHandle ETH;
         [ReadOnly]
         public CollisionWorld CollisionWorld;
 
         [BurstCompile]
         public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
         {
+            int baseEntityIndex = ChunkBaseEntityIndices[unfilteredChunkIndex];
+            Debug.Log($"baseEntityIndex = {baseEntityIndex}");
+            return;
+            var enumerator = new ChunkEntityEnumerator(useEnabledMask, chunkEnabledMask, chunk.Count);
+            while (enumerator.NextEntityIndex(out var i))
+            {
+                Debug.Log($"i = {i}");
+            }
+            var array = chunk.GetNativeArray(ETH);
+            foreach (var item in array)
+            {
+                Debug.Log($"item.Index = {item.Index}, unfilteredChunkIndex = {unfilteredChunkIndex}");
+            }
             var accessor = chunk.GetBufferAccessor<SpawnInstructionBuffer>(ref SpawnInstructionsTypeHandle);
             for (int i = 0; i < accessor.Length; i++)
             {
@@ -81,7 +105,7 @@ public partial struct SpawnPerInstructionSystem : ISystem
         [BurstCompile]
         private void GetRandomPosition(ref float3 setPosition, SpawnInstructionBuffer instruction)
         {
-            var random = Random.CreateFromIndex(instruction.RandomSeed);
+            var random = Unity.Mathematics.Random.CreateFromIndex(instruction.RandomSeed);
             int tries = 10;
             var collisionFilter = new CollisionFilter { BelongsTo = uint.MaxValue, CollidesWith = uint.MaxValue };
             var distanceHits = new NativeList<DistanceHit>(Allocator.Temp);
