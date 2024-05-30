515f844475ead07ad81d37dca84a2b5c0324310c
Mark Weeber <m.webber@inbox.ru>
2024-05-30 16:49:18 +0600

    small changes - trying to make entity spawn upon item drop from inventory to work

--- Assets/Scripts/Components/Item/ItemAuthoring.cs
+++ Assets/Scripts/Components/Item/ItemAuthoring.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ItemAuthoring : MonoBehaviour
 {
+	[SerializeField] private GameObject prefab;
 	class Baker : Baker<ItemAuthoring>
 	{
 		public override void Bake(ItemAuthoring authoring)
@@ -12,11 +13,12 @@ public class ItemAuthoring : MonoBehaviour
             {
 				_item.InitializeActions();
                 Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+				Entity prefabEntity = GetEntity(authoring.prefab, TransformUsageFlags.Dynamic);
 				_item.PrefabEntity = entity;
                 AddComponentObject(entity, new ItemData
 				{
 					Item = _item,
-					PrefabEntity = entity,
+					PrefabEntity = prefabEntity,
 				});
 			}
 		}
